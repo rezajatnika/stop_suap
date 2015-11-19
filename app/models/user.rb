566fc5c3191c64db1f87f63fccf9655cd9dc5e20
Reza Jatnika <rezajatnika@gmail.com>
2015-11-20 04:25:12 +0700

    Remove username column

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,6 @@ class User < ActiveRecord::Base
 
     # Some validations
     c.merge_validates_length_of_password_field_options(in: 8..30)
-
-    c.merge_validates_length_of_login_field_options(in: 5..25)
-    c.merge_validates_format_of_login_field_options(
-      with: /\A[a-zA-Z0-9_][a-zA-Z0-9\.+\-_@]+\z/,
-      message: 'should use only letters, numbers, and .-_@'
-    )
-
     c.merge_validates_length_of_email_field_options(maximum: 50)
     c.merge_validates_format_of_email_field_options(
       message: 'should use valid email address format'
--- db/migrate/20151119175057_create_users.rb
+++ db/migrate/20151119175057_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username,            null: false, index: true, unique: true
       t.string :email,               null: false, index: true, unique: true
       t.string :crypted_password,    null: false
       t.string :password_salt,       null: false

97fc4f9e798f37f4b3af7071b1be48da7174a1c8
Reza Jatnika <rezajatnika@gmail.com>
2015-11-20 04:55:47 +0700

    Remove username test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(
-      username: 'username',
       email: 'username@email.com',
       password: 'asdqwe123',
       password_confirmation: 'asdqwe123'
@@ -12,22 +11,6 @@ class UserTest < ActiveSupport::TestCase
 
   test 'should valid' do
     assert @user.valid?
-  end
-
-  test 'should be valid with length of username in 5..25' do
-    @user.username = 'five5'
-    assert @user.valid?
-
-    @user.username = 'five5' * 5
-    assert @user.valid?
-  end
-
-  test 'should not valid with length of username < 5 or > 25' do
-    @user.username = 'four'
-    assert_not @user.valid?
-
-    @user.username = 'five5' * 26
-    assert_not @user.valid?
   end
 
   test 'should not valid with length of email > 50' do

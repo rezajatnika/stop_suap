2b0ec1bc1b0887a2416698cecd1c9656fb7a1924
Reza Jatnika <rezajatnika@gmail.com>
2015-11-23 08:36:22 +0700

    Add data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(
-  email: 'rezajatnika@gmail.com',
+  email: 'user@example.net',
   password: 'asdqwe123',
   password_confirmation: 'asdqwe123'
+)
+
+west_java = Province.create(name: 'West Java')
+west_java.cities.create(
+  [
+    { name: 'Kota Bandung' },
+    { name: 'Kota Cimahi' },
+    { name: 'Kabupaten Bandung' }
+  ]
+)
+
+Category.create(
+  [
+    { name: 'Tax Office' },
+    { name: 'Traffic Ticket' },
+    { name: 'Public Service' }
+  ]
 )

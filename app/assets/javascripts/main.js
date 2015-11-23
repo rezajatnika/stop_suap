862f7efb9fef0b17bc669e74da5c726e7217f8af
Reza Jatnika <rezajatnika@gmail.com>
2015-11-24 05:02:15 +0700

    Move javascript tag to bottom

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,13 @@
 // Close alert message
-$(document).on('ready page:load', function() {
-  $("a.alert-close").click(function(event) {
-    event.preventDefault();
-    $(".alert-flash").hide();
-  });
+$("a.alert-close").click(function(event) {
+  event.preventDefault();
+  $(".alert-flash").hide();
 });
 
 // Pick a date
-$(document).ready(function() {
-  $('#story_event_date').pickadate({
-    today: '',
-    clear: '',
-    close: '',
-    max: new Date()
-  });
+$('#story_event_date').pickadate({
+  today: '',
+  clear: '',
+  close: '',
+  max: new Date()
 });

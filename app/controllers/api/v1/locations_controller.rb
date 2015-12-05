23da6d9bb64d5f2d0d6b27c40a7427dc14f07ddb
Reza Jatnika <rezajatnika@gmail.com>
2015-12-06 03:36:27 +0700

    Parallel experimental

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,18 +3,21 @@ class Api::V1::LocationsController < ApplicationController
     respond_to do |format|
       format.json { render json: get_map_data }
     end
+    Province.connection.reconnect!
   end
 
   def pie_data
     respond_to do |format|
       format.json { render json: get_pie_data }
     end
+    Category.connection.reconnect!
   end
 
   def province_data
     respond_to do |format|
       format.json { render json: get_province_data }
     end
+    Category.connection.reconnect!
   end
 
   private
@@ -23,18 +26,24 @@ class Api::V1::LocationsController < ApplicationController
   def get_map_data
     @provinces = Province.all
     @locations = Location.includes(:province)
-    @provinces.map do |province|
+    Parallel.map(@provinces, in_threads: 2) do |province|
       {
         'hc-key': province.code,
         'value': @locations.where(province_id: province.id).count
       }
     end.unshift({ 'hc-key': 'id-3700', 'value': 0 })
+    # @provinces.map do |province|
+    #   {
+    #     'hc-key': province.code,
+    #     'value': @locations.where(province_id: province.id).count
+    #   }
+    # end.unshift({ 'hc-key': 'id-3700', 'value': 0 })
   end
 
   # FIXME: Move to model or helper
   def get_pie_data
     @categories = Category.includes(:stories).all
-    @categories.map do |category|
+    Parallel.map(@categories, in_threads: 2) do |category|
       {
         'name': category.name,
         'y': Story.count.zero? ?
@@ -42,6 +51,14 @@ class Api::V1::LocationsController < ApplicationController
         'drilldown': category.name
       }
     end
+    # @categories.map do |category|
+    #   {
+    #     'name': category.name,
+    #     'y': Story.count.zero? ?
+    #       0 : category.stories.count.to_f / Story.count * 100,
+    #     'drilldown': category.name
+    #   }
+    # end
   end
 
   # FIXME: Move to model or helper
@@ -49,7 +66,7 @@ class Api::V1::LocationsController < ApplicationController
     @categories = Category.includes(:stories).all
     @provinces = Province.all
 
-    @categories.map do |category|
+    Parallel.map(@categories, in_threads: 2) do |category|
       {
         'name': category.name,
         'id': category.name,
@@ -63,6 +80,21 @@ class Api::V1::LocationsController < ApplicationController
         end
       }
     end
+
+    # @categories.map do |category|
+    #   {
+    #     'name': category.name,
+    #     'id': category.name,
+    #     'data': @provinces.map do |province|
+    #       [
+    #         province.name,
+    #         category.stories.count.zero? ?
+    #           0 : story_count(province, category) /
+    #               category.stories.count.to_f * 100
+    #       ]
+    #     end
+    #   }
+    # end
   end
 
   def story_count(province, category)

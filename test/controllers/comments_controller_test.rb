a8a127b3631b82cb4f15eb8f8073301ff06b8685
Reza Jatnika <rezajatnika@gmail.com>
2015-12-05 19:14:05 +0700

    Add bunch of controller tests
    - Story test
    - Comment test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    UserSession.create(users(:theuser))
+    @story = stories(:first)
+  end
+
+  test 'should post comment' do
+    assert_difference('Comment.count') do
+      post :create, comment: { body: 'Test' }, story_id: @story.id, format: :js
+    end
+  end
 end
--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -13,12 +13,52 @@ class StoriesControllerTest < ActionController::TestCase
   end
 
   test 'should post story anonymously' do
-    # ...
+    assert_difference('Story.count') do
+      post :create, story: {
+        title: 'Title of The Title',
+        content: 'Summary of the story.',
+        category_id: categories(:tax).id,
+        paid: 'paid',
+        amount: 25000,
+        event_date: Date.today,
+        name: 'Reza Jatnika',
+        email: 'reza@rakarsa.com',
+        location_attributes: {
+          province_id: provinces(:westjava).id,
+          city_id: cities(:bandung).id
+        }
+      }
+    end
+
+    story = assigns(:story)
+    assert_redirected_to story
   end
 
   class LoggedInUser < StoriesControllerTest
+    def setup
+      UserSession.create(users(:theuser))
+    end
+
     test 'should post story' do
-      # ...
+      assert_difference('Story.count') do
+        post :create, story: {
+          title: 'Title of The Title',
+          content: 'Summary of the story.',
+          category_id: categories(:tax).id,
+          paid: 'paid',
+          amount: 25000,
+          event_date: Date.today,
+          location_attributes: {
+            province_id: provinces(:westjava).id,
+            city_id: cities(:bandung).id
+          }
+        }
+      end
+
+      story = assigns(:story)
+      assert_equal users(:theuser).id, story.user_id
+      assert_equal users(:theuser).email, story.email
+      assert_redirected_to story
     end
   end
 end

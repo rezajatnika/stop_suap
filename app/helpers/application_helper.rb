cd2297b8fff299cdff0049b80183839a84ee3d1d
azmy <azmy.ansori@gmail.com>
2015-12-07 01:02:59 +0700

    Provide page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,18 @@
 module ApplicationHelper
   
+  # Add class="active" for current page
   def active_class(link_path)
     current_page?(link_path) ? "active" : ""
+  end
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Stop Suap"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
   
 end
